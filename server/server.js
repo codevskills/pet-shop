a11231ece9083582e80ed999a9115c7f271c9e0c
codevskills <ccschools.edu@gmail.com>
2023-10-28 10:32:03 +0530

    B

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ const PORT = process.env.PORT || 5000;
 app.use("/", requirementContoller);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at the PORT: ${PORT}`);
+  console.log(`Server listening at the PORT: ${PORT}`);
 });
